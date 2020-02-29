aeb7193b7d3953ef21dce892eef9d886968e7a02
Arkan8 <adrianromerot88@gmail.com>
2020-02-29 15:23:44 +0100

    29-02-2020 15:23

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -34,7 +34,7 @@
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    {{ __('Enviar enlace para restablece la contraseña') }}
+                                    {{ __('Enviar enlace para restablecer la contraseña') }}
                                 </button>
                             </div>
                         </div>
